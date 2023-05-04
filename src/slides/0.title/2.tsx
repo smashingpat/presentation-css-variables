8a47cadc2e7123240f24b31013c68c46d006e8ef
Patrick Gerritsen <p.gerritsen@wearetriple.com>
2023-05-04 13:16:40 +0200

    feat: add slide

---
+++ src/slides/0.title/2.tsx
@@ -0,0 +1,10 @@
+import Stack from '~src/lib/components/Stack';
+import Text from '~src/lib/components/Text';
+
+export default function Slide() {
+  return (
+    <Stack fill centered vertical>
+      <Text variant="mega-title">Cascading Style Sheets</Text>l
+    </Stack>
+  );
+}
